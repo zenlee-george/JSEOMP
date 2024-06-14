606ebce6b6e9ce790ba3c5bda941de5343f37012
zenleegeorge <zenlee.george1@gmail.com>
2024-06-14 23:30:55 +0200

    admin

--- Scripts/products.js
+++ Scripts/products.js
@@ -1,3 +1,26 @@
+// Navbar
+function togglemenu() {
+  document.querySelectorAll(".nav-container").forEach((item) => {
+    item.classList.toggle("show-menu");
+  });
+  document.querySelectorAll(".nav").forEach((item) => {
+    item.classList.toggle("show-menu");
+  });
+  document.querySelector(".navbutton").classList.toggle("show-menu");
+}
+
+document.querySelector(".navbutton").addEventListener("mouseover", () => {
+  document.querySelector(".bar-top").classList.add("move");
+  document.querySelector(".bar-middle").classList.add("move");
+  document.querySelector(".bar-bottom").classList.add("move");
+});
+
+document.querySelector(".navbutton").addEventListener("mouseout", () => {
+  document.querySelector(".bar-top").classList.remove("move");
+  document.querySelector(".bar-middle").classList.remove("move");
+  document.querySelector(".bar-bottom").classList.remove("move");
+});
+
 // Functionality
 function CreateItem(id, name, category, image, description, price, currency) {
   this.id = id
